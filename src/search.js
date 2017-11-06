f49af61171eb44f470129c295365e599ab8a8754
Andrew Wybo <andrewwybo@gmail.com>
2017-11-06 17:05:12 -0500

    console logging Eventful title, url, description, and event start time

--- src/search.js
+++ src/search.js
@@ -15,15 +15,15 @@ client.searchEvents({ keywords: 'ucf' }, function(err, data){
      
      console.log('Recieved ' + data.search.total_items + ' events');
      
-     console.log('Event listings: ' + JSON.stringify(data.search.events.event[0].title));
+     //console.log('Event listings: ' + JSON.stringify(data.search.events.event[0]));
      
      //print the title of each event 
-    //  for (var i=0; i < data.length; i++){
-    //     console.log("Results: " + JSON.stringify(objectName)data[i])
-    //  }
-     
-    //  }
-    
+     //url
+     //description
+     //start_time
+     for (var i=0; i < data.search.events.event.length; i++){
+       console.log("Title: " + data.search.events.event[i].title + "\n" + "URL: " + data.search.events.event[i].url+ "\n" + "Description: " + data.search.events.event[i].description + "\n" + "Start Time: " + data.search.events.event[i].start_time);
+     }
    });
 
 
