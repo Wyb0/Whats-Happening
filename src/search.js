bdfd598c56f70fc3ab4e93af9d43d8ae6df20f4f
Andrew Wybo <andrewwybo@gmail.com>
2017-11-06 16:36:26 -0500

    added npm package for eventful, results pulling for keywords

--- src/search.js
+++ src/search.js
@@ -1,44 +1,69 @@
 //const eventfulKey = require("apiKeys.js")
 //const twitterKeys = require("./apiKeys.js")
 //const twitter = require("twitter")
+const eventful = require('eventful-node');
+const client = new eventful.Client("HczB5FQVwXz7KhLN");
 
-$(document).ready(function(){
-    // new change: eventful api
+
+client.searchEvents({ keywords: 'ucf' }, function(err, data){
+    
+     if(err){
+     
+       return console.error(err);
+     
+     }
+     
+     console.log('Recieved ' + data.search.total_items + ' events');
+     
+     console.log('Event listings: ' + JSON.stringify(data.search.events.event[0].title));
+     
+     //print the title of each event 
+    //  for (var i=0; i < data.length; i++){
+    //     console.log("Results: " + JSON.stringify(objectName)data[i])
+    //  }
+     
+    //  }
+    
+   });
+
+
+// $(document).ready(function(){
+//     // new change: eventful api
     
 
-    const searchTerm = "ucf"
-    const eventfulKey = "HczB5FQVwXz7KhLN"
+//     const searchTerm = "ucf"
+//     const eventfulKey = "HczB5FQVwXz7KhLN"
 
-    function pullLocation() {
+//     function pullLocation() {
 
-        const eventfulURL = "http://api.eventful.com/json/events/search?app_key=" + eventfulKey + "&keywords=" + searchTerm
+//         const eventfulURL = "http://api.eventful.com/json/events/search?app_key=" + eventfulKey + "&keywords=" + searchTerm
 
-        $.ajax({
-            url: eventfulURL,
-            dataType: 'jsonp',
-            method: "GET"
-        }).done(response => {
-            //Path for Event
-            console.log(response)
+//         $.ajax({
+//             url: eventfulURL,
+//             dataType: 'jsonp',
+//             method: "GET"
+//         }).done(response => {
+//             //Path for Event
+//             console.log(response)
             
-            for (var i=0; i < response.events.event.length; i++){
-                console.log(response.events.event[i].title)
+//             for (var i=0; i < response.events.event.length; i++){
+//                 console.log(response.events.event[i].title)
 
-            }
-            //console.log(response.events.event[i])
-        })
-    }
+//             }
+//             //console.log(response.events.event[i])
+//         })
+//     }
     
-    // function twitterPull(){
+//     // function twitterPull(){
 
-    //     const client = new Twitter(twitterKeys)
+//     //     const client = new Twitter(twitterKeys)
 
-    //     client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
-    //         console.log(tweets);
-    //     });
-    // }
+//     //     client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+//     //         console.log(tweets);
+//     //     });
+//     // }
 
-    pullLocation();
-    //twitterPull();
+//     pullLocation();
+//     //twitterPull();
 
-})
+// })
