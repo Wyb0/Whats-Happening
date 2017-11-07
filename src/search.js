ac2c5cd53c7e0d03d876d4fbbef3823ff12d328b
Andrew Wybo <andrewwybo@gmail.com>
2017-11-07 12:45:41 -0500

    added npm  twit to package.json, wrapped calls in function

--- src/search.js
+++ src/search.js
@@ -4,29 +4,31 @@ const NodeGeocoder = require('node-geocoder');
 
 const options = {
   provider: 'google',
- 
-  // Optional depending on the providers
-  httpAdapter: 'https', // Default
-  //apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
-  //formatter: "string"         // 'gpx', 'string', ...
+  httpAdapter: 'https',
 };
 
 const geocoder = NodeGeocoder(options);
+let input ="ucf"
 
-geocoder.geocode('ucf', function(error, data) {
-  console.log("Address: " + data[0].formattedAddress + "\nLatitude: " + data[0].latitude + "\nLongitude" + data[0].longitude);
-});
+function search (input) {
+  geocoder.geocode(input, function(error, data) {
+    console.log("Address: " + data[0].formattedAddress + "\nLatitude: " + data[0].latitude + "\nLongitude" + data[0].longitude);
+    let lat = data[0].latitude
+    // console.log("Lat: " + lat)
+    let long = data[0].longitude
+    
+  });
 
-client.searchEvents({ keywords: 'ucf' }, function(err, data){
+  client.searchEvents({ keywords: 'ucf' }, function(err, data){
     if(err){
         return console.error(err);
-     }
-     //console.log('Event listings: ' + JSON.stringify(data.search.events.event[0]));
-     //print the title of each event, url, description, start_time
-     for (var i=0; i < data.search.events.event.length; i++){
-       console.log("Title: " + data.search.events.event[i].title + "\nURL: " + data.search.events.event[i].url+ "\nDescription: " + data.search.events.event[i].description + "\nStart Time: " + data.search.events.event[i].start_time);
-     }
-   });
+    }
+    //console.log('Event listings: ' + JSON.stringify(data.search.events.event[0]));
+    //print the title of each event, url, description, start_time
+    for (var i=0; i < data.search.events.event.length; i++){
+      console.log("Title: " + data.search.events.event[i].title + "\nURL: " + data.search.events.event[i].url+ "\nDescription: " + data.search.events.event[i].description + "\nStart Time: " + data.search.events.event[i].start_time);
+    }
+  });
+}
 
-
-
+search(input)
